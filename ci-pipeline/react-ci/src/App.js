631fb2260ba5e660c5030daabc2ab12fc0b8dc65
majo1569 <ojedamariaj1569@gmail.com>
2021-06-28 14:53:54 -0300

    first commit

---
+++ ci-pipeline/react-ci/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+      return (
+        <div className="App w-100 bg-light">
+          <Login/>
+        </div>
+             );
+  
+}
+
+export default App;
